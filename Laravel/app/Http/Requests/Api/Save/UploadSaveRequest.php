4f74093849852539a0e5a354183034fd897f307d
Savayaqu <zcomn@mail.ru>
2025-05-28 15:14:51 +0700

    fix: UploadSaveRequest.php

--- Laravel/app/Http/Requests/Api/Save/UploadSaveRequest.php
+++ Laravel/app/Http/Requests/Api/Save/UploadSaveRequest.php
@@ -12,8 +12,8 @@ class UploadSaveRequest extends ApiRequest
             'file_size' => 'required|integer',
             'file_name' => 'required|string',
             'version' => 'required|string',
-            'side_game_id' => 'nullable|integer|exists:libraries,side_game_id|required_without:game_id',
-            'game_id' => 'nullable|integer|exists:libraries,game_id|required_without:side_game_id',
+            'side_game_id' => 'nullable|integer|exists:libraries,side_game_id|required_without:game_id|prohibits:game_id',
+            'game_id' => 'nullable|integer|exists:libraries,game_id|required_without:side_game_id|prohibits:side_game_id',
             'description' => 'nullable|string',
         ];
     }
