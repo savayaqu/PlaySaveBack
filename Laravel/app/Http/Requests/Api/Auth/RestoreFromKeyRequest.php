4668e6f5cc2714de3423c9a428cfdbc3cd32f808
Savayaqu <zcomn@mail.ru>
2025-06-05 23:23:50 +0700

    fix: key is random string

--- Laravel/app/Http/Requests/Api/Auth/RestoreFromKeyRequest.php
+++ Laravel/app/Http/Requests/Api/Auth/RestoreFromKeyRequest.php
@@ -10,7 +10,7 @@ class RestoreFromKeyRequest extends ApiRequest
     {
         return [
             'login' => 'required|string|exists:users,login',
-            'key' => 'required|integer',
+            'key' => 'required|string',
             'new_password' => 'required|string|min:6|confirmed',
             'new_password_confirmation' => 'required|string|min:6',
             'logout' => 'boolean',
